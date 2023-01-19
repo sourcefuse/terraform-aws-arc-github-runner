ee0e921ac1efacfa3dee6911bc9b4815c8bd3552
Travis Saucier <travis.saucier@sourcefuse.com>
2023-01-18 20:22:08 -0500

    reformat to avoid race condition

--- locals.tf
+++ locals.tf
@@ -1,6 +1,12 @@
 locals {
-  runner_ssm_association = [
-    "AWS-UpdateSSMAgent",
-    aws_ssm_document.runner.name
+  ec2_runner_ssm_association = [
+    {
+      name = "AWS-UpdateSSMAgent"
+    },
+    {
+      name = aws_ssm_document.runner.name
+    }
   ]
+
+  aws_friendly_runner_labels = replace(var.runner_labels, ",", " + ")
 }
